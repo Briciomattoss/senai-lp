9a35a7235f5696af6dea782c0ebadbcd628eff6c
Fabrício Matos De Sousa <>
2024-09-24 21:51:10 -0300

    html + js

--- livrojs/cap02/js/exe2_1.js
+++ livrojs/cap02/js/exe2_1.js
@@ -1,1 +1,10 @@
-alert("teste se carregou o arquivo js k")
+// criar uma referência ao forme e ao h3 (onde será exibida a resposta)
+const frm = document.querySelector("form");
+const resp = document.querySelector("h3");
+
+// cria um 'ouvinte' de evento, acionado quando o botão submit for clicado
+frm.addEventListener("submit", (e)=>{
+    const nome = frm.inNome.value; // obtém o nome digitado no form
+    resp.innerText = `Olá ${nome}`; // exibe a resposta do progama
+    e.preventDefault(); // vai evitar que o form envio os dados pro destino.php
+});
