a8fe54eb2fbeef3aabf34e6d23003f0ff6a1bd80
Fabrício Matos De Sousa <>
2024-09-27 19:27:00 -0300

    Ajustes

--- livrojs/cap02/js/exe2_1.js
+++ livrojs/cap02/js/exe2_1.js
@@ -1,15 +1,12 @@
-// criar uma referência ao forme e ao h3 (onde será exibida a resposta)
+// criar uma referencia ao form e ao h3 (onde será exibida a resposta)
 const frm = document.querySelector("form");
 const resp = document.querySelector("h3");
 
 // cria um 'ouvinte' de evento, acionado quando o botão submit for clicado
-frm.addEventListener("submit", executar (e)=>{
+frm.addEventListener("submit", executar);
 
-
-});
-
-function executar(params) {
-    const nome = frm.inNome.value; // obtém o nome digitado no form
-    resp.innerText = `Olá <i>${nome}</i>`; // exibe a resposta do progama
-    evento.proventDefault(); //vai evitar que o form envio os dados pro destino.php
+function executar(evento) {
+    const nome = frm.inNome.value; // obtém o nome digitado no form 
+    resp.innerHTML = `Olá <i>${nome}</i>`; // exibe a respota do programa
+    evento.preventDefault();
 }
