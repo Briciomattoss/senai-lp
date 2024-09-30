4af00d56dfe38bc4a7619a28cf0ac6f13d98fbf4
Fabrício Matos De Sousa <>
2024-09-30 19:14:33 -0300

    ajuste

--- livrojs/cap02/js/exe2_3.js
+++ livrojs/cap02/js/exe2_3.js
@@ -4,8 +4,8 @@ const resp2 = document.querySelector("#outResp2");
 const resp3 = document.querySelector("#outResp3");
 
 frm.addEventListener("submit", (e)=>{
-    const veiculo = frm.inVeiculo.value;
-    const preco = Number(frm.inPreco.value);
+    const veiculo = frm.inNomeDoVeiculo.value;
+    const preco = Number(frm.inPrecoDoVeiculo.value);
 
     const entrada = preco * 0.50;
     const parcela = (preco * 0.50) / 12;
@@ -13,5 +13,5 @@ frm.addEventListener("submit", (e)=>{
     resp1.innerText = `Promoção: ${veiculo}`;
     resp2.innerText = `Entrada de R$: ${entrada.toFixed(2)}`;
     resp3.innerText = `+12x de R$ ${parcela.toFixed(2)}`;
-
+    e.preventDefault();
 })
