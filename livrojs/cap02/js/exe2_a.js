1e4e9cb7580d0c2c79c9e885aeaf7c45c8f0122f
Fabrício Matos De Sousa <briciomattoss@gmail.com>
2024-10-02 21:51:13 -0300

    Exemplo 4.2

--- livrojs/cap02/js/exe2_a.js
+++ livrojs/cap02/js/exe2_a.js
@@ -1,16 +1,16 @@
-const frm = document.querySelector("outform");
-const resp = document.querySelector("#Resp1");
+const frm = document.querySelector("form");
+const resp1 = document.querySelector("#outResp1");
 const resp2 = document.querySelector("#outResp2");
 
-frm.addEventListener("submit", (e)=>{
-    const medicamento = frm.inMedicamneto.value;
+frm.addEventListener("submit", (e) => {
+    e.preventDefault(); // Mover para o início da função
+
+    const medicamento = frm.inNomeDoMedicamento.value; // Corrigir o nome do campo
     const preco = Number(frm.inPreco.value);
 
     const promocao = Math.floor(preco * 2);
 
-
     resp1.innerText = `Promoção de ${medicamento}`;
     resp2.innerText = `Leve 2 por apenas R$: ${promocao.toFixed(2)}`;
-e.preventDefault();
 
 })
---
+++ livrojs/cap02/js/exe2_b.js
@@ -0,0 +1,9 @@
+const frm = document.querySelector("form");
+const resp = document.querySelector("h3)");
+
+frm = addEventListener("submit", (e)=>{
+
+    const valor = Number(frm.inValor.value);
+    const tempo = Number(frm.inTempo.value);
+
+})
