9daaac906e1d3089412f746fc29922783e6e7e3d
Fabrício Matos De Sousa <briciomattoss@gmail.com>
2024-12-06 21:45:10 -0300

    ajustes do exercício

--- bd/relatorio-campeonatobr.sql
+++ bd/relatorio-campeonatobr.sql
@@ -99,25 +99,61 @@ update partida set gol_mandante = 3 , gol_visitante = 1 where id_partida = 347;
 update partida set gol_mandante = 0 , gol_visitante = 0 where id_partida = 348;
 update partida set gol_mandante = 0 , gol_visitante = 0 where id_partida = 349;
 update partida set gol_mandante = 1 , gol_visitante = 1 where id_partida = 350;
-
-
-update partida set gol_mandante = 0 , gol_visitante = 1 where id_partida = 351;
-update partida set gol_mandante = 0 , gol_visitante = 1 where id_partida = 352;
-update partida set gol_mandante = 0 , gol_visitante = 1 where id_partida = 353;
-update partida set gol_mandante = 0 , gol_visitante = 1 where id_partida = 354;
-update partida set gol_mandante = 0 , gol_visitante = 1 where id_partida = 355;
-update partida set gol_mandante = 0 , gol_visitante = 1 where id_partida = 356;
-update partida set gol_mandante = 0 , gol_visitante = 1 where id_partida = 357;
-update partida set gol_mandante = 0 , gol_visitante = 1 where id_partida = 358;
-update partida set gol_mandante = 0 , gol_visitante = 1 where id_partida = 359;
-update partida set gol_mandante = 0 , gol_visitante = 1 where id_partida = 360;
+update partida set gol_mandante = 2 , gol_visitante = 3 where id_partida = 351;
+update partida set gol_mandante = 1 , gol_visitante = 3 where id_partida = 352;
+update partida set gol_mandante = 1 , gol_visitante = 2 where id_partida = 353;
+update partida set gol_mandante = 2 , gol_visitante = 4 where id_partida = 354;
+update partida set gol_mandante = 2 , gol_visitante = 2 where id_partida = 355;
+update partida set gol_mandante = 2 , gol_visitante = 1 where id_partida = 356;
+update partida set gol_mandante = 3 , gol_visitante = 2 where id_partida = 357;
+update partida set gol_mandante = 2 , gol_visitante = 0 where id_partida = 358;
+update partida set gol_mandante = 1 , gol_visitante = 1 where id_partida = 359;
+update partida set gol_mandante = 1 , gol_visitante = 1 where id_partida = 360;
 update partida set gol_mandante = 0 , gol_visitante = 1 where id_partida = 361;
-update partida set gol_mandante = 0 , gol_visitante = 1 where id_partida = 362;
-update partida set gol_mandante = 0 , gol_visitante = 1 where id_partida = 363;
+update partida set gol_mandante = 1 , gol_visitante = 2 where id_partida = 362;
+update partida set gol_mandante = 1 , gol_visitante = 1 where id_partida = 363;
+update partida set gol_mandante = 1 , gol_visitante = 0 where id_partida = 364; 
+update partida set gol_mandante = 2 , gol_visitante = 0 where id_partida = 365; 
+update partida set gol_mandante = 3 , gol_visitante = 0 where id_partida = 366; 
+update partida set gol_mandante = 1 , gol_visitante = 2 where id_partida = 367; 
+update partida set gol_mandante = 1 , gol_visitante = 2 where id_partida = 368; 
+update partida set gol_mandante = 3 , gol_visitante = 1 where id_partida = 369; 
+update partida set gol_mandante = 0 , gol_visitante = 3 where id_partida = 370;
 
 
 
 
+update partida set gol_mandante = 0 , gol_visitante = 1 where id_partida = 371;
+update partida set gol_mandante = 0 , gol_visitante = 1 where id_partida = 372;
+update partida set gol_mandante = 0 , gol_visitante = 1 where id_partida = 373;
+update partida set gol_mandante = 0 , gol_visitante = 1 where id_partida = 374;
+update partida set gol_mandante = 0 , gol_visitante = 1 where id_partida = 375;
+update partida set gol_mandante = 0 , gol_visitante = 1 where id_partida = 376;
+update partida set gol_mandante = 0 , gol_visitante = 1 where id_partida = 377;
+update partida set gol_mandante = 0 , gol_visitante = 1 where id_partida = 378;
+update partida set gol_mandante = 0 , gol_visitante = 1 where id_partida = 379;
+update partida set gol_mandante = 0 , gol_visitante = 1 where id_partida = 380;
 
 
+-- 03
+select * from evento;
+group by descricao;
+
+select minuto, COUNT(*) quantidade from evento
+where -- descricao like 'Gol%' and descricao not like '%Var&'
+descricao in ('Gol (Gol de campo)','Gol (Pênalti)',' Gol (Gol Contra)')
+group by minuto
+order by quantidade desc;
+
+
+-- 04 
+
+select * from casa;
+
+
+
+
+-- 05
+
+select id_jogador, SUM(if(descricao = 'Cartão Amarelo' 
 
